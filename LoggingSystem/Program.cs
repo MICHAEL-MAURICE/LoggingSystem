f64ebb09a4ae15a5f495f910e8b1e71a37954afe
Michael Maurice <M.Maurice@klickit.co>
2024-07-20 01:38:57 +0300

    adding Docker Compos File , serilogPackages and swagger

--- LoggingSystem/Program.cs
+++ LoggingSystem/Program.cs
@@ -4,9 +4,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+// Register the Swagger generator, defining one or more Swagger documents
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    // Enable middleware to serve generated Swagger as a JSON endpoint.
+    app.UseSwagger();
+
+    // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+    // specifying the Swagger JSON endpoint.
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty; // To serve Swagger UI at the app's root
+    });
+}
 
 app.UseHttpsRedirection();
 
